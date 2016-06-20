406596641bf227c2ee9071790167777d9431396d
liuzhenrong <liu.zhenrong@jyall.com>
2016-06-20 18:07:15 +0800

    刚开始

---
+++ app/src/main/java/com/liu/glory/MainActivity.java
@@ -0,0 +1,185 @@
+package com.liu.glory;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.turn.ttorrent.client.Client;
+import com.turn.ttorrent.client.SharedTorrent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.security.NoSuchAlgorithmException;
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    public static final String DOWNLOAD_PATH =
+            Environment.getExternalStorageDirectory().getAbsolutePath()+
+                    "/downloads/";
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MyTask().execute();
+            }
+        });
+
+    }
+
+    private class MyTask extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            startDownload();
+            return null;
+        }
+    }
+
+    private void startDownload(){
+        // TODO Auto-generated method stub
+        // First, instantiate the Client object.
+        Client client;
+        try {
+            client = new Client(
+                    // This is the interface the client will listen on (you might need something
+                    // else than localhost here).
+                    InetAddress.getLocalHost(),
+
+                    // Load the torrent from the torrent file and use the given
+                    // output directory. Partials downloads are automatically recovered.
+                    SharedTorrent.fromFile(
+                            new File(DOWNLOAD_PATH+"test.torrent"),
+                            new File(DOWNLOAD_PATH)));
+            // You can optionally set download/upload rate limits
+            // in kB/second. Setting a limit to 0.0 disables rate
+            // limits.
+            client.setMaxDownloadRate(50.0);
+            client.setMaxUploadRate(50.0);
+
+            // At this point, can you either call download() to download the torrent and
+            // stop immediately after...
+            client.download();
+
+            // Or call client.share(...) with a seed time in seconds:
+            // client.share(3600);
+            // Which would seed the torrent for an hour after the download is complete.
+
+            // Downloading and seeding is done in background threads.
+            // To wait for this process to finish, call:
+            client.waitForCompletion();
+            // At any time you can call client.stop() to interrupt the download.
+            client.addObserver(new Observer() {
+
+                @Override
+                public void update(Observable observable, Object arg) {
+                    // TODO Auto-generated method stub
+                    Client client = (Client) observable;
+                    float progress = client.getTorrent().getCompletion();
+                    // Do something with progress.
+                    System.out.println("progress="+progress);
+                }
+            });
+        } catch (NoSuchAlgorithmException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
